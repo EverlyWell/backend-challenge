40a9b057521e853aaa6eb60d7334fb877bb363fc
rfrm <rfrm>
2021-06-10 13:31:13 -0500

    feat: show member profile after creation

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,8 +4,19 @@ class MembersController < ApplicationController
   end
 
   def show
+    @member = Member.find params[:id]
   end
 
   def create
+    @member = Member.new member_params
+    if @member.save
+      redirect_to @member
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :url)
   end
 end
