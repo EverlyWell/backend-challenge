c02777f61a7e0a9306c0185edce6139a0026bb01
Ignacio Alonso <ignacio.alley@gmail.com>
2021-06-30 21:03:36 -0500

    Add the member shorten_url service

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,9 +4,13 @@ class Member < ApplicationRecord
   validates :first_name, :last_name, :url, presence: true 
 
   after_create :scrape_info
-
+  after_save :shorten_url, if: :saved_change_to_url?
 
   def scrape_info
     HeadingScraper.execute(self)
+  end
+
+  def shorten_url
+    BitlyUrlShortener.execute(self)
   end
 end
--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -4,6 +4,9 @@ FactoryBot.define do
     last_name  { "Alonso" }
     url        { "https://ignacio.al/" }
 
-    after(:build) { |obj| obj.class.skip_callback(:create, :after, :scrape_info, raise: false) }
+    after(:build) do |obj| 
+      obj.class.skip_callback(:create, :after, :scrape_info, raise: false)
+      obj.class.skip_callback(:save, :after, :shorten_url, raise: false)
+    end
   end
 end
