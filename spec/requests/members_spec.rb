a40eaf1f14d9e096b0eee342dfdfe215f1962291
Ignacio Alonso <ignacio.alley@gmail.com>
2021-06-30 22:15:21 -0500

    Create the member endpoints

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -1,6 +1,7 @@
 describe 'Members', type: :request do
   let(:body) { JSON.parse(response.body) }
   let(:headers) { { "Accept" => "application/json", 'Content-Type' => 'application/json' } }
+  let(:vcr_record_mode) { :none }
 
   describe 'creating a member' do
     subject { post '/members', params: params.to_json, headers: headers }
@@ -17,8 +18,10 @@ describe 'Members', type: :request do
       end
 
       it 'returns the correct status code' do
-        subject
-        expect(response).to have_http_status(:success)
+        VCR.use_cassette("create_member_request", record: vcr_record_mode) do
+          subject
+          expect(response).to have_http_status(:success)
+        end
       end
     end
 
@@ -47,6 +50,8 @@ describe 'Members', type: :request do
   end
 
   describe 'viewing a member' do
+    let!(:member) { create(:member) }
+
     context 'when member exists' do
       subject { get "/members/#{Member.first.id}", headers: headers }
 
@@ -56,7 +61,7 @@ describe 'Members', type: :request do
       end
     end
 
-    context 'when member not fond' do
+    context 'when member not found' do
       subject { get '/members/0', headers: headers }
 
       it 'returns the correct status code' do
