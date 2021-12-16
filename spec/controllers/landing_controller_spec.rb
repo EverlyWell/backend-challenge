e8e8ba120ef63f1abdf54783aaa5cb339ed0c638
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2021-12-16 16:16:30 -0600

    Add Landing controller as root
    Substitute default rails welcome page

---
+++ spec/controllers/landing_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe LandingController, type: :controller do
+  render_views
+
+  describe 'GET #show' do
+    let(:get_show) do
+      get :show
+    end
+
+    it 'responds with HTTP 200 Ok' do
+       expect(get_show).to have_http_status(:ok)
+    end
+  end
+end
